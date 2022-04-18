2e254f86347cbaafa666b8300097ab7520170e06
Ryoma Nishizawa <amoyr@RyomanoMacBook-Air.local>
2022-04-18 21:16:53 +0900

    I create returning html

---
+++ server8086.js
@@ -0,0 +1,14 @@
+const http = require("http")
+const server = http.createServer()
+const fs = require("fs")
+const basePath = process.cwd()
+
+server.on("request", doRequest)
+server.listen(8086)
+
+function doRequest(request, response){
+  const requestedPath = request.url
+  const responseFile = fs.readFileSync(basePath + requestedPath, "utf-8")
+  response.write(responseFile)
+  response.end()
+}
